7394b8b65800dca1fe025ca8ac1efae4cf55617b
Leelavathi Gade <leelaroshangade@gmail.com>
2022-01-24 21:12:01 +0100

    changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -62,56 +62,73 @@ export default function Login({setUser,user, setToken, isLoggedIn, setIsLoggedIn
    
 
     return (
-    <div className="login-container">
-      <h2 className="heading2">Welcome to the Language community</h2>
-      {!isLoggedIn && <h4 className="login-h">Log in  </h4>}
-       {/* <span onClick={onClick}>Login </span> if don't have account please <Link to="/signup" > Sign up</Link> here */}
-      
-     
-        <form  >
-        <div className="form-div"> 
-        <TextField
-        id="filled-basic"
-        label="Email"
-        variant="filled"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        className="email-textfield"
-        style={{ marginBottom: "20px",marginTop:"20px", width:"300px", height:"50px"}}
-        />
-        <TextField
-        id="filled-basic"
-        label="Password"
-        type="password"
-        variant="filled"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        style={{ marginBottom: "20px",marginTop:"20px", width:"300px"}}
-       />
-       <FormGroup>
-       <FormControlLabel 
-       control={<Checkbox defaultChecked  color="primary"/>} 
-       label="Remember Me"
-       
-       />
-       </FormGroup>
-             
-        <Button  type="submit"
-        variant="contained"   
-        onClick={handleSubmit}
-        style={{ color: "teal",marginTop:"30px",marginBottom:"30px",backgroundColor:"#5CE1E6", width:"300px" }} >
-        Log In</Button>
+      <div className="login-container">
+        <h2 className="heading2">Welcome to the Language community</h2>
+        {!isLoggedIn && <h4 className="login-h">Log in </h4>}
+        {/* <span onClick={onClick}>Login </span> if don't have account please <Link to="/signup" > Sign up</Link> here */}
 
+        <form>
+          <div className="form-div">
+            <TextField
+              id="filled-basic"
+              label="Email"
+              variant="filled"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="email-textfield"
+              style={{
+                marginBottom: '20px',
+                marginTop: '20px',
+                width: '300px',
+                height: '50px',
+              }}
+            />
+            <TextField
+              id="filled-basic"
+              label="Password"
+              type="password"
+              variant="filled"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              style={{
+                marginBottom: '20px',
+                marginTop: '20px',
+                width: '300px',
+              }}
+            />
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    // defaultChecked
+                    // color="#5CE1E6"
+                    style={{ color: '#5CE1E6', marginLeft:'2vw'}}
+                  />
+                }
+                label="Remember Me"
+              />
+            </FormGroup>
 
-       <Link to="/signup"> 
-        <p className="signup-link">Don't have an account Please Signup</p>
-        </Link>
- 
-        </div>
+            <Button
+              type="submit"
+              variant="contained"
+              onClick={handleSubmit}
+              style={{
+                color: 'teal',
+                marginTop: '30px',
+                marginBottom: '30px',
+                backgroundColor: '#5CE1E6',
+                width: '300px',
+              }}
+            >
+              Log In
+            </Button>
+
+            <Link to="/signup">
+              <p className="signup-link">Don't have an account Please Signup</p>
+            </Link>
+          </div>
         </form>
-
-     
-       
-   </div>
-    )
+      </div>
+    );
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -98,113 +98,161 @@ export default function Signup({ user,setUser, setIsLoggedIn }) {
     };
 
   return (
+    <div className="signup-container">
+      <h2 className="heading2">Sign Up </h2>
 
-  <div className="signup-container">
-    <h2 className="heading2">Sign Up </h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-container">
+          <TextField
+            id="username"
+            label="User name"
+            variant="filled"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            style={{
+              marginBottom: '20px',
+              marginTop: '20px',
+              width: '300px',
+              height: '50px',
+            }}
+          />
 
-    <form  onSubmit={handleSubmit}>
+          <TextField
+            label="Email"
+            variant="filled"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="email-textfield"
+            style={{
+              marginBottom: '20px',
+              marginTop: '20px',
+              width: '300px',
+              height: '50px',
+            }}
+          />
 
-    <div className="form-container"> 
+          <TextField
+            label="Password"
+            variant="filled"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{
+              marginBottom: '20px',
+              marginTop: '20px',
+              width: '300px',
+              height: '50px',
+            }}
+          />
+          {/* <p>Your Native Language </p><br></br> */}
 
-     <TextField
-		 id="username"
-		 label="User name"
-		 variant="filled"
-		 value={userName}
-		 onChange={(e) => setUserName(e.target.value)}
-     
-		style={{ marginBottom: "20px",marginTop:"20px", width:"300px", height:"50px"}}
-		/>
+          {languages.map((language, index) => {
+            if (!index) {
+              return (
+                <FormControl
+                  key={index}
+                  variant="filled"
+                  sx={{ m: 1, minWidth: 10 }}
+                >
+                  <InputLabel id="NativeLanguage">
+                    {' '}
+                    your native language
+                  </InputLabel>
+                  <Select
+                    labelId="NativeLanguage"
+                    onChange={(e) => handleChangeLanguage(e, index)}
+                    style={{
+                      marginBottom: '20px',
+                      marginTop: '20px',
+                      width: '300px',
+                      height: '50px',
+                    }}
+                  >
+                    {Languages.map((item, i) => (
+                      <MenuItem key={i} value={item.name}>
+                        {item.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              );
+            } else {
+              return (
+                <React.Fragment key={index}>
+                  {index === 1 ? <p>Languages To Practice</p> : ''}
+                  <FormControl variant="filled" sx={{ m: 1, minWidth: 10 }}>
+                    <InputLabel id="demo-simple-select-filled-label">
+                      practice language
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-filled-label"
+                      id="demo-simple-select-filled"
+                      onChange={(e) => handleChangeLanguage(e, index)}
+                      style={{
+                        marginBottom: '20px',
+                        marginTop: '20px',
+                        width: '300px',
+                        height: '50px',
+                      }}
+                    >
+                      {Languages.map((item, i) => (
+                        <MenuItem key={i} value={item.name}>
+                          {item.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
 
-    <TextField
-		
-		label="Email"
-		variant="filled"
-		value={email}
-		onChange={(e) => setEmail(e.target.value)}
-    className="email-textfield"
-		style={{ marginBottom: "20px",marginTop:"20px", width:"300px", height:"50px"}}
-		/>
+                  <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+                    <InputLabel id="PracticeLanguage">level</InputLabel>
+                    <Select
+                      labelId="PracticeLanguage"
+                      onChange={(e) => handleChangeLevel(e, index)}
+                      style={{
+                        marginBottom: '20px',
+                        marginTop: '20px',
+                        width: '300px',
+                        height: '50px',
+                      }}
+                    >
+                      <MenuItem value="">
+                        <em>None</em>
+                      </MenuItem>
+                      {levels.map((level, i) => (
+                        <MenuItem key={i} value={level._id}>
+                          {level.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </React.Fragment>
+              );
+            }
+          })}
 
-    <TextField
-		label="Password"
-		variant="filled"
-    type="password"
-		value={password}
-		onChange={(e) => setPassword(e.target.value)}
-    style={{ marginBottom: "20px",marginTop:"20px", width:"300px",height:"50px"}}
-		/>
-    {/* <p>Your Native Language </p><br></br> */}
-        
-    {languages.map((language, index) => {
-      if (!index) {
-        return (
-          <FormControl key={index} variant="filled" sx={{ m: 1, minWidth: 10 }}>
-          <InputLabel id="NativeLanguage"> your native language</InputLabel>
-          <Select
-          labelId="NativeLanguage"
-          onChange={(e) => handleChangeLanguage(e, index)}
-          style={{ marginBottom: "20px",marginTop:"20px", width:"300px", height:"50px"}}
-          >{Languages.map((item, i)=> (
-          <MenuItem key={i} value={item.name}>{item.name}</MenuItem>
-          ))}
-          </Select>
-         </FormControl>
-        )
-      } else {
-        return (
-        <React.Fragment key={index}>
-        {index === 1 ? <p>Languages To Practice</p> : ''}
-        <FormControl variant="filled" sx={{ m: 1, minWidth: 10 }}>
-        <InputLabel id="demo-simple-select-filled-label">practice language</InputLabel>
-        <Select
-        labelId="demo-simple-select-filled-label"
-        id="demo-simple-select-filled"
-        onChange={(e) => handleChangeLanguage(e, index)}
-        style={{ marginBottom: "20px",marginTop:"20px", width:"300px", height:"50px"}}
-        >{Languages.map((item, i)=> (
-        <MenuItem key={i} value={item.name}>{item.name}</MenuItem>
-        ))}
-        </Select>
-       </FormControl>
-  
-       <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="PracticeLanguage">level</InputLabel>
-        <Select
-        labelId="PracticeLanguage"
-        onChange={(e) => handleChangeLevel(e, index)}
-        style={{ marginBottom: "20px",marginTop:"20px", width:"300px", height:"50px"}}
-        >
-         <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            {levels.map((level, i) => (
-              <MenuItem key={i} value={level._id}>{level.name}</MenuItem>
-            ))}
-        </Select>
-       </FormControl>
-       </React.Fragment>
-       )
-      }
-    })}
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Checkbox  style={{ color: '#5CE1E6 ', marginLeft:'2vw' }} />
+              }
+              label=" I agree to the terms and conditions"
+            />
 
-      <FormGroup>
-       <FormControlLabel 
-       control={<Checkbox defaultChecked color="primary"/>} 
-       label=" I agree to the terms and conditions"
-      />
-
-            
-      <Button type="submit"
-      variant="contained"   
-      style={{ color: "#1F4E5A",backgroundColor:"#5CE1E6", width:"300px",height:"50px" }} >
-		  Sign Up</Button>
-      </FormGroup>
-
+            <Button
+              type="submit"
+              variant="contained"
+              style={{
+                color: '#1F4E5A',
+                backgroundColor: '#5CE1E6',
+                width: '300px',
+                height: '50px',
+              }}
+            >
+              Sign Up
+            </Button>
+          </FormGroup>
         </div>
-     </form>
+      </form>
     </div>
-
-
-    )
+  );
 }
